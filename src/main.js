2dee0288d1eeccbd5efea2cfc0d4f631a577aef8
waterricecake <fruturum@nate.com>
2024-11-14 16:57:30 +0900

    feat :  implement string router and mockdatas

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import settingRouter from './settings/router/SettingsRouter.js';
 import structureRouter from './structure/router/StructureRouter.js'
+import stringRouter from './strings/router/StringRouter.js'
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -10,6 +11,7 @@ app.use(cors());
 
 app.use('/settings', settingRouter);
 app.use('/structures', structureRouter);
+app.use('/strings', stringRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '빈 포트에서 대기 중');
