2e44669119449b9318ec7dea5ea78270a51ff83b
waterricecake <fruturum@nate.com>
2024-11-14 16:25:06 +0900

    feat : implement structure router and set mock data

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import router from './settings/router/SettingsRouter.js';
+import settingRouter from './settings/router/SettingsRouter.js';
+import structureRouter from './structure/router/StructureRouter.js'
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(cors());
 
-app.use('/', router);
+app.use('/settings', settingRouter);
+app.use('/structures', structureRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '빈 포트에서 대기 중');
