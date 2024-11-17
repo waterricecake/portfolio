c370a53f5fd53c5be102a925c583afedd99576d8
waterricecake <fruturum@nate.com>
2024-11-18 00:30:57 +0900

    refactor : delete unuse code

--- src/articles/route/ArticlesRouter.js
+++ src/articles/route/ArticlesRouter.js
@@ -1,15 +1,7 @@
 import express from 'express';
-import fs from 'fs/promises';
 import structureService from '../application/StructureService.js';
 
 const router = express.Router();
-
-const getMockData = async (filename) => {
-  const data = await fs.readFile(
-    new URL(`./../mock/${filename}`, import.meta.url),
-  );
-  return JSON.parse(data);
-};
 
 router.get('/article/:articleId', async (req, res) => {
   const mock = await structureService.get(req.params.articleId);
--- src/structures/route/StructureRouter.js
+++ src/structures/route/StructureRouter.js
@@ -1,15 +1,7 @@
 import express from 'express';
-import fs from 'fs/promises';
 import structureService from '../application/StructureService.js';
 
 const router = express.Router();
-
-const getMockData = async (filename) => {
-  const data = await fs.readFile(
-    new URL(`./../mock/${filename}`, import.meta.url),
-  );
-  return JSON.parse(data);
-};
 
 router.get('/:userId', async (req, res) => {
   const mock = await structureService.getStructure(req.params.userId);
